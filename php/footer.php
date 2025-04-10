69947e9b6e0b760c352a8bb514818ab2b6d1bb7e
sakanafurai <info@via.rossa.cc>
2025-04-10 22:48:24 +0900

    Add print mode footer

--- php/footer.php
+++ php/footer.php
@@ -1,8 +1,22 @@
 <footer>
+
+  <?php if ( isset( $_GET['print'] ) ): ?>
+
+  <!-- Print mode footer -->
+  <p class="copyright">
+  <?php echo $site->title(); ?> (<i><?php echo $page->permalink() ?></i>)
+  </p>
+
+  <?php else: ?>
+
+  <!-- Navbar -->
    <?php include(THEME_DIR_PHP.'navbar.php'); ?>
 
+  <!-- Footer credits -->
   <p class="copyright">
     <?php echo $site->title(); ?> <?php echo $site->footer(); ?><br>
     Powered by <a target="_blank" href="https://www.bludit.com">BLUDIT</a>
   </p>
+  <?php endif; ?>
+
 </footer>
